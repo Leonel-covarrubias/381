0e3dd57bc871ee4f17a0999c825fa6536f561522
PackNel87 <packnel87@Ubuntu22.10.myguest.virtualbox.org>
2024-02-24 19:21:14 -0800

    as3 done

---
+++ as3/readMe.txt
@@ -0,0 +1,32 @@
+git submodule add https://github.com/joshuadahlunr/raylib-cpp.git
+
+
+
+For Compiling: 
+
+Delete Build folder
+
+mkdir build> cd build > cmake > make 
+
+
+
+
+
+
+
+To run: 
+
+(in the build directory)
+
+./as3
+
+enumarate controls: 
+
+w: speed up
+s: slow down 
+a: turn right 
+d: turn left
+q: increase altitude 
+e: decrease altitude
+space: stop moving selected plane
+tab: Switch selected plane 
--- as3/src/as3.cpp
+++ as3/src/as3.cpp
@@ -55,6 +55,8 @@ int main(){
     float speed3 = 0; 
     float acceleration = 20; 
 
+    float flightspeed1, flightspeed2,flightspeed3= 0; 
+
     raylib::Vector3 position1 = {0,0,0}; 
     raylib::Vector3 position2 = {50,0,0}; 
     raylib::Vector3 position3 = {-50,0,0}; 
@@ -100,13 +102,13 @@ int main(){
                     return t.Translate(position3).RotateY(heading3).Scale(1,1,1);
                 },3,currentPlane);
 
-                raylib::Vector3 velocity1 = {speed1 *cos(heading1.RadianValue()), 0, -speed1 *sin(heading1.RadianValue())}; 
+                raylib::Vector3 velocity1 = {speed1 *cos(heading1.RadianValue()), flightspeed1, -speed1 *sin(heading1.RadianValue())}; 
                 position1 += velocity1 * window.GetFrameTime();
 
-                raylib::Vector3 velocity2 = {speed2 *cos(heading2.RadianValue()), 0, -speed2 *sin(heading2.RadianValue())}; 
+                raylib::Vector3 velocity2 = {speed2 *cos(heading2.RadianValue()), flightspeed2, -speed2 *sin(heading2.RadianValue())}; 
                 position2 += velocity2 * window.GetFrameTime();
 
-                raylib::Vector3 velocity3 = {speed3 *cos(heading3.RadianValue()), 0, -speed3 *sin(heading3.RadianValue())}; 
+                raylib::Vector3 velocity3 = {speed3 *cos(heading3.RadianValue()), flightspeed3, -speed3 *sin(heading3.RadianValue())}; 
                 position3 += velocity3 * window.GetFrameTime();
 
 
@@ -140,18 +142,18 @@ int main(){
                     
                 }
                 if (IsKeyDown(KEY_Q)) {
-                    speed2 += acceleration * GetFrameTime();
-                    velocity1.y = speed2;
+                    flightspeed1 += acceleration * GetFrameTime();
+                   
                 }
                 if (IsKeyDown(KEY_E)) {
-                    speed2 -= acceleration * GetFrameTime();
-                    velocity1.y = speed2;
+                    flightspeed1 -= acceleration * GetFrameTime();
                 }
                 if (IsKeyPressed(KEY_SPACE)) {
                     velocity1.x = 0; 
                     velocity1.y = 0;
                     velocity1.z = 0;
-                    speed1 = 0; } 
+                    speed1 = 0;
+                    flightspeed1 = 0; } 
                     
                 break;
             case 2:
@@ -172,18 +174,18 @@ int main(){
                     
                 }
                 if (IsKeyDown(KEY_Q)) {
-                    speed2 += acceleration * GetFrameTime();
-                    velocity1.y = speed2;
+                    flightspeed2 += acceleration * GetFrameTime();
+                   
                 }
                 if (IsKeyDown(KEY_E)) {
-                    speed2 -= acceleration * GetFrameTime();
-                    velocity1.y = speed2;
+                     flightspeed2 -= acceleration * GetFrameTime();
                 }
                 if (IsKeyPressed(KEY_SPACE)) {
                     velocity2.x = 0; 
                     velocity2.y = 0;
                     velocity2.z = 0;
-                    speed2 = 0; } 
+                    speed2 = 0;
+                    flightspeed2 = 0;  } 
                 break;
             case 3:
                if (IsKeyDown(KEY_A)) {
@@ -201,19 +203,19 @@ int main(){
                     speed3 -= acceleration * GetFrameTime();
                     
                 }
-                if (IsKeyDown(KEY_Q)) {
-                    speed2 += acceleration * GetFrameTime();
-                    velocity1.y = speed2;
+               if (IsKeyDown(KEY_Q)) {
+                    flightspeed3 += acceleration * GetFrameTime();
+                   
                 }
                 if (IsKeyDown(KEY_E)) {
-                    speed2 -= acceleration * GetFrameTime();
-                    velocity1.y = speed2;
+                     flightspeed3 -= acceleration * GetFrameTime();
                 }
                 if (IsKeyPressed(KEY_SPACE)) {
                     velocity3.x = 0; 
                     velocity3.y = 0;
                     velocity3.z = 0;
-                    speed3 = 0; } 
+                    speed3 = 0;
+                    flightspeed3 = 0; } 
                 break;
         }
 
